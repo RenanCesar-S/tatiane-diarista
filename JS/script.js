bcc20e9ae2d384ed89bfd82283ec36ec076602e8
Renan Cesar <107968834+RenanCesar-S@users.noreply.github.com>
2023-02-04 19:56:08 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+function whatsapp() {
+    setTimeout(()=>{
+        window.open("https://wa.me/+5519982360913?text=Ol%C3%A1%20Tatiane,%20gostaria%20de%20marcar%20um%20hor%C3%A1rio!!!", "_blank");
+    }, 500)
+}
